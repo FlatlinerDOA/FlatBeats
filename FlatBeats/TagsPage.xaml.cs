80be6c48ac416c33589dad53663b86f13523ccbc
Andrew Chisholm <andrewc@dnstech.com.au>
2011-09-06 03:28:03 -1000

    Added animation to item groups

--- FlatBeats/TagsPage.xaml.cs
+++ FlatBeats/TagsPage.xaml.cs
@@ -17,6 +17,8 @@ namespace FlatBeats
 
     public partial class TagsPage : PhoneApplicationPage
     {
+        private LongListSelector currentSelector;
+
         public TagsPage()
         {
             InitializeComponent();
@@ -51,6 +53,114 @@ namespace FlatBeats
             if (navItem != null && navItem.NavigationUrl != null)
             {
                 this.NavigationService.Navigate(navItem.NavigationUrl);
+            }
+        }
+
+        private void LongListSelector_GroupViewOpened(object sender, GroupViewOpenedEventArgs e)
+        {
+            //Hold a reference to the active long list selector.
+            currentSelector = sender as LongListSelector;
+
+            //Construct and begin a swivel animation to pop in the group view.
+            IEasingFunction quadraticEase = new QuadraticEase { EasingMode = EasingMode.EaseOut };
+            Storyboard _swivelShow = new Storyboard();
+            ItemsControl groupItems = e.ItemsControl;
+
+            foreach (var item in groupItems.Items)
+            {
+                UIElement container = groupItems.ItemContainerGenerator.ContainerFromItem(item) as UIElement;
+                if (container != null)
+                {
+                    Border content = VisualTreeHelper.GetChild(container, 0) as Border;
+                    if (content != null)
+                    {
+                        DoubleAnimationUsingKeyFrames showAnimation = new DoubleAnimationUsingKeyFrames();
+
+                        EasingDoubleKeyFrame showKeyFrame1 = new EasingDoubleKeyFrame();
+                        showKeyFrame1.KeyTime = TimeSpan.FromMilliseconds(0);
+                        showKeyFrame1.Value = -60;
+                        showKeyFrame1.EasingFunction = quadraticEase;
+
+                        EasingDoubleKeyFrame showKeyFrame2 = new EasingDoubleKeyFrame();
+                        showKeyFrame2.KeyTime = TimeSpan.FromMilliseconds(85);
+                        showKeyFrame2.Value = 0;
+                        showKeyFrame2.EasingFunction = quadraticEase;
+
+                        showAnimation.KeyFrames.Add(showKeyFrame1);
+                        showAnimation.KeyFrames.Add(showKeyFrame2);
+
+                        Storyboard.SetTargetProperty(showAnimation, new PropertyPath(PlaneProjection.RotationXProperty));
+                        Storyboard.SetTarget(showAnimation, content.Projection);
+
+                        _swivelShow.Children.Add(showAnimation);
+                    }
+                }
+            }
+
+            _swivelShow.Begin();
+        }
+
+        private void LongListSelector_GroupViewClosing(object sender, GroupViewClosingEventArgs e)
+        {
+            //Cancelling automatic closing and scrolling to do it manually.
+            e.Cancel = true;
+            if (e.SelectedGroup != null)
+            {
+                currentSelector.ScrollToGroup(e.SelectedGroup);
+            }
+
+            //Dispatch the swivel animation for performance on the UI thread.
+            Dispatcher.BeginInvoke(() =>
+            {
+                //Construct and begin a swivel animation to pop out the group view.
+                IEasingFunction quadraticEase = new QuadraticEase { EasingMode = EasingMode.EaseOut };
+                Storyboard swivelHide = new Storyboard();
+                ItemsControl groupItems = e.ItemsControl;
+
+                foreach (var item in groupItems.Items)
+                {
+                    UIElement container = groupItems.ItemContainerGenerator.ContainerFromItem(item) as UIElement;
+                    if (container != null)
+                    {
+                        Border content = VisualTreeHelper.GetChild(container, 0) as Border;
+                        if (content != null)
+                        {
+                            DoubleAnimationUsingKeyFrames showAnimation = new DoubleAnimationUsingKeyFrames();
+
+                            EasingDoubleKeyFrame showKeyFrame1 = new EasingDoubleKeyFrame();
+                            showKeyFrame1.KeyTime = TimeSpan.FromMilliseconds(0);
+                            showKeyFrame1.Value = 0;
+                            showKeyFrame1.EasingFunction = quadraticEase;
+
+                            EasingDoubleKeyFrame showKeyFrame2 = new EasingDoubleKeyFrame();
+                            showKeyFrame2.KeyTime = TimeSpan.FromMilliseconds(125);
+                            showKeyFrame2.Value = 90;
+                            showKeyFrame2.EasingFunction = quadraticEase;
+
+                            showAnimation.KeyFrames.Add(showKeyFrame1);
+                            showAnimation.KeyFrames.Add(showKeyFrame2);
+
+                            Storyboard.SetTargetProperty(showAnimation, new PropertyPath(PlaneProjection.RotationXProperty));
+                            Storyboard.SetTarget(showAnimation, content.Projection);
+
+                            swivelHide.Children.Add(showAnimation);
+                        }
+                    }
+                }
+
+                swivelHide.Completed += this.SwivelHideCompleted;
+                swivelHide.Begin();
+
+            });          
+        }
+
+        private void SwivelHideCompleted(object sender, EventArgs e)
+        {
+            //Close group view.
+            if (this.currentSelector != null)
+            {
+                this.currentSelector.CloseGroupView();
+                this.currentSelector = null;
             }
         }
     }
