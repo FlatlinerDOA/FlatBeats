5d5ac2cc71eafca1fd43a203fe60f8264fa477f3
Andrew Chisholm <andrewc@dnstech.com.au>
2012-01-06 03:42:11 -1100

    - Updated error message

--- FlatBeatsPlaybackAgent/AudioPlayer.cs
+++ FlatBeatsPlaybackAgent/AudioPlayer.cs
@@ -216,7 +216,7 @@ namespace FlatBeatsPlaybackAgent
             return playNextTrack.Catch<Unit, Exception>(
                 ex =>
                     {
-                        var data = "PlayNextTrack error, stopping!";
+                        var data = "Error playing next track, we have to stop!\r\n";
                         if (this.NowPlaying != null)
                         {
                             data += Json<PlayingMixContract>.Serialize(this.NowPlaying);
