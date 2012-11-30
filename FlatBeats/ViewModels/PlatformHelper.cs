4c2e0adc22c001319701d158f6dc1ee37010113b
Chiz <flatliner60@hotmail.com>
2012-11-30 01:53:17 -1100

    Added support for Windows Phone 7.8 and 8.0 wide and small live tiles

---
+++ FlatBeats/ViewModels/PlatformHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace FlatBeats.ViewModels
+{
+    using System;
+
+    public static class PlatformHelper
+    {
+        private static Version TargetedVersion8 = new Version(8, 0);
+
+        private static Version TargetedVersion78 = new Version(7, 10, 8858); 
+
+        public static bool IsWindowsPhone8OrLater { get { return Environment.OSVersion.Version >= TargetedVersion8; } }
+
+        public static bool IsWindowsPhone78OrLater { get { return Environment.OSVersion.Version >= TargetedVersion78; } }
+    }
+}
